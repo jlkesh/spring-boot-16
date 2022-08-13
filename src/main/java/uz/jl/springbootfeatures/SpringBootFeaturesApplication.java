4e8d1ea248075476c3344fc57b13d4fc201bbf49
Javohir <john.lgd65@gmail.com>
2022-08-13 10:06:12 +0500

    initial

---
+++ src/main/java/uz/jl/springbootfeatures/SpringBootFeaturesApplication.java
@@ -0,0 +1,13 @@
+package uz.jl.springbootfeatures;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFeaturesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootFeaturesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/jl/springbootfeatures/SpringBootFeaturesApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.jl.springbootfeatures;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFeaturesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
