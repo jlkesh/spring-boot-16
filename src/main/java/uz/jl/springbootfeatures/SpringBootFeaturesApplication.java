be5e3159b6385c79fb5fddc619ea7f6355fa8c76
Javohir <john.lgd65@gmail.com>
2022-08-23 11:49:49 +0500

    go go

--- src/main/java/uz/jl/springbootfeatures/SpringBootFeaturesApplication.java
+++ src/main/java/uz/jl/springbootfeatures/SpringBootFeaturesApplication.java
@@ -1,9 +1,15 @@
 package uz.jl.springbootfeatures;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
 import org.apache.coyote.http11.AbstractHttp11Protocol;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -20,6 +26,32 @@ public class SpringBootFeaturesApplication {
         SpringApplication.run(SpringBootFeaturesApplication.class, args);
     }
 
+    @Bean
+    public ServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+            @Override
+            protected void postProcessContext(Context context) {
+                var securityConstraint = new SecurityConstraint();
+                securityConstraint.setUserConstraint("CONFIDENTIAL");
+                var collection = new SecurityCollection();
+                collection.addPattern("/*");
+                securityConstraint.addCollection(collection);
+                context.addConstraint(securityConstraint);
+            }
+        };
+        tomcat.addAdditionalTomcatConnectors(getHttpConnector());
+        return tomcat;
+    }
+
+    private Connector getHttpConnector() {
+        var connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+        connector.setScheme("http");
+        connector.setPort(8080);
+        connector.setSecure(false);
+        connector.setRedirectPort(8443);
+        return connector;
+    }
+
 
 }
 
@@ -30,24 +62,6 @@ class FileUploadController {
     @GetMapping("/upload")
     public String getFileUploadPage() {
         return "upload";
-    }
-
-    @PostMapping("/upload")
-    public String upload(@RequestPart(name = "file") MultipartFile file, RedirectAttributes redirectAttributes) {
-        redirectAttributes.addFlashAttribute("message", "file uploaded");
-        System.out.println(file.getName());
-        return "redirect:/upload";
-    }
-//    @GetMapping("/download")
-}
-
-@ControllerAdvice
-class GlobalExceptionHandler  {
-
-    @ExceptionHandler(MaxUploadSizeExceededException.class)
-    public String handleMultipartException(MaxUploadSizeExceededException e, RedirectAttributes redirectAttributes) {
-        redirectAttributes.addFlashAttribute("error", e.getMessage());
-        return "redirect:/upload";
     }
 
 }
