436cb6168dd693941032e45adbd4a4a6dfc44b33
Javohir <john.lgd65@gmail.com>
2022-08-24 11:52:39 +0500

    go go

--- src/test/java/uz/jl/springbootfeatures/SpringBootFeaturesApplicationTests.java
+++ src/test/java/uz/jl/springbootfeatures/SpringBootFeaturesApplicationTests.java
@@ -1,93 +1,36 @@
 package uz.jl.springbootfeatures;
 
-import com.fasterxml.jackson.annotation.JsonAnyGetter;
-import com.fasterxml.jackson.annotation.JsonGetter;
-import com.fasterxml.jackson.annotation.JsonPropertyOrder;
-import com.fasterxml.jackson.annotation.JsonRootName;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import lombok.AllArgsConstructor;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import uz.jl.springbootfeatures.models.Book;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.Comparator;
 import java.util.Objects;
 
 @SpringBootTest
 class SpringBootFeaturesApplicationTests {
 
-    @Autowired
-    private ObjectMapper objectMapper;
-
     @Test
-    void checkForKeyExistanse() throws JsonProcessingException {
-        JacksonTestController jacksonTestController = new JacksonTestController();
-        List<Book> objects = jacksonTestController.getObjects();
-        String data = objectMapper.writeValueAsString(objects);
-        //Assertions.assertThat(data).contains("PageCount");
-    }
-
-    @Test
-    void checkForKeyNonExistanse() throws JsonProcessingException {
-        JacksonTestController jacksonTestController = new JacksonTestController();
-        List<Book> objects = jacksonTestController.getObjects();
-        String data = objectMapper.writeValueAsString(objects);
-//        assertThat(data).contains("sfet243t43");
-    }
-
-    @Test
-    public void whenSerializingUsingJsonAnyGetter_thenCorrect()
-            throws JsonProcessingException {
-
-        ExtendableBean bean = new ExtendableBean("My bean");
-        bean.getProperties().put("attr1", "val1");
-        bean.getProperties().put("attr2", "val2");
-        String result = new ObjectMapper().writeValueAsString(bean);
-        System.out.println(result);
+    void requireTest() {
+        Object o = null;
+        //Objects.requireNonNull(o);
+        //Objects.requireNonNull(o, "Object can not be null");
+        //Supplier<String> supplier = () -> "Object can not be null";
+        //Objects.requireNonNull(o, supplier);
+        //Object o1 = Objects.requireNonNullElse(o, "Default Data String");
+        //System.out.println("o1 = " + o1);
+        //Supplier<String> supplier = () -> "Object can not be null";
+        //Object res = Objects.requireNonNullElseGet(o, supplier);
+        //System.out.println("res = " + res);
     }
 
 
     @Test
-    public void whenSerializingUsingJsonGetter_thenCorrect()
-            throws JsonProcessingException {
-        MyBean bean = new MyBean(1, "My bean");
-        objectMapper.enable(SerializationFeature.WRAP_ROOT_VALUE);
-        String result = objectMapper.writeValueAsString(bean);
-        System.out.println(result);
+    void checkIndexTest() {
+        // a , n
+        //Objects.checkIndex()
+        Object a = 1;
+        Object b = 2;
+        Objects.compare(a, b, (o1, o2) -> 0);
     }
 
-
-    static class ExtendableBean {
-        public String name;
-
-        public ExtendableBean(String name) {
-            this.name = name;
-        }
-
-        private Map<String, String> properties = new HashMap<>();
-
-        @JsonAnyGetter
-        public Map<String, String> getProperties() {
-            return properties;
-        }
-    }
-
-
-    @JsonRootName(value = "data")
-    @JsonPropertyOrder({"my_name", "id"})
-    @AllArgsConstructor
-    public class MyBean {
-        public int id;
-        private String name;
-
-        @JsonGetter("my_name")
-        public String getTheName() {
-            return name;
-        }
-    }
 }
