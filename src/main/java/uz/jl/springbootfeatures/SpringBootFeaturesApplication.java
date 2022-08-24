5c2def4ef17b402c3f6207c5a54cc798b7b00493
Javohir <john.lgd65@gmail.com>
2022-08-24 10:13:11 +0500

    go go

--- src/main/java/uz/jl/springbootfeatures/SpringBootFeaturesApplication.java
+++ src/main/java/uz/jl/springbootfeatures/SpringBootFeaturesApplication.java
@@ -1,13 +1,141 @@
 package uz.jl.springbootfeatures;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.core.MessageSendingOperations;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @SpringBootApplication
+@Controller
+@RequiredArgsConstructor
 public class SpringBootFeaturesApplication {
+
+    private final MessageSendingOperations<String> messageTemplate;
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootFeaturesApplication.class, args);
+    }
+
+    @GetMapping
+    public String indexPage() {
+        return "index";
+    }
+
+    @ResponseBody
+    @GetMapping("test")
+    public String test(@RequestParam(required = false, defaultValue = "ME") String name) {
+        String time = new SimpleDateFormat("HH:mm").format(new Date());
+
+        OutputMessage message = new OutputMessage(name, "Raise message", time);
+        messageTemplate.convertAndSend("/topic/messages", message);
+        return name;
+    }
+
+    @MessageMapping("/chat")
+    @SendTo("/topic/messages")
+    public OutputMessage send(Message message) throws Exception {
+        String time = new SimpleDateFormat("HH:mm").format(new Date());
+        return new OutputMessage(message.getFrom(), message.getText(), time);
+    }
+
+}
+
+@Configuration
+@EnableWebSocketMessageBroker
+class WebSocketConfigurer implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/chat").withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/topic/", "/queue/");
+        config.setApplicationDestinationPrefixes("/app");
+    }
+}
+
+class Message {
+    private String from;
+    private String text;
+
+    public Message() {
+    }
+
+    public Message(String from, String text) {
+        this.from = from;
+        this.text = text;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
+
+class OutputMessage {
+    private String from;
+    private String text;
+    private String time;
+
+    public OutputMessage() {
+
+    }
+
+    public OutputMessage(String from, String text, String time) {
+        this.from = from;
+        this.text = text;
+        this.time = time;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
     }
 
 }
