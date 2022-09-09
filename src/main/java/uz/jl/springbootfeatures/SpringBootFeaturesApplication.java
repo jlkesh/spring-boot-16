9fe6de5db2d126a369faa5158d0c1722282aae7c
Javohir <john.lgd65@gmail.com>
2022-09-09 18:21:14 +0500

    go go

--- src/main/java/uz/jl/springbootfeatures/SpringBootFeaturesApplication.java
+++ src/main/java/uz/jl/springbootfeatures/SpringBootFeaturesApplication.java
@@ -1,9 +1,23 @@
 package uz.jl.springbootfeatures;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.webmvc.RepositoryRestController;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @SpringBootApplication
+@OpenAPIDefinition
 public class SpringBootFeaturesApplication {
 
     public static void main(String[] args) {
@@ -11,3 +25,22 @@ public class SpringBootFeaturesApplication {
     }
 
 }
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String title;
+    private String author;
+}
+
+@RepositoryRestResource(path = "kitob",collectionResourceRel = "kitob")
+interface BookRepository extends JpaRepository<Book, Long> {
+
+}
+
