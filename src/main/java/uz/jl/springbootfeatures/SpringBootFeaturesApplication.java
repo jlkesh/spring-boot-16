2824a7733dd941f7bb807500429255f7db65d4bf
Javohir <john.lgd65@gmail.com>
2022-09-15 19:17:11 +0500

    go go

--- src/main/java/uz/jl/springbootfeatures/SpringBootFeaturesApplication.java
+++ src/main/java/uz/jl/springbootfeatures/SpringBootFeaturesApplication.java
@@ -6,11 +6,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.hibernate.annotations.GenericGenerator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.CacheConfig;
-import org.springframework.cache.annotation.Cacheable;
-import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.redis.core.RedisHash;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.HashOperations;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.*;
 
 import javax.persistence.Entity;
@@ -18,12 +20,26 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import java.io.Serializable;
 import java.util.List;
-import java.util.function.Supplier;
+import java.util.UUID;
 
 @SpringBootApplication
 @OpenAPIDefinition
-@EnableCaching
 public class SpringBootFeaturesApplication {
+
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration("localhost", 6379);
+        return new LettuceConnectionFactory(redisStandaloneConfiguration);
+    }
+
+    @Bean
+    public RedisTemplate<String, Book> redisTemplate() {
+        RedisTemplate<String, Book> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        return redisTemplate;
+    }
+
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootFeaturesApplication.class, args);
@@ -35,29 +51,25 @@ public class SpringBootFeaturesApplication {
 @RestController
 @RequiredArgsConstructor
 @Slf4j
-@CacheConfig(cacheNames = "book")
-@RedisHash(value = "book", timeToLive = 2)
 class BookController {
     final BookRepository repository;
 
     @GetMapping
     public List<Book> getAll() {
         log.info("Get All BOOKS api called");
-        return repository.findAll();
-    }
-
-    @GetMapping("/{id}")
-    @Cacheable(key = "#id")
-    public Book getAll(@PathVariable String id) {
-        log.info("GET ONE BOOK api called");
-        Supplier<RuntimeException> supplier = () -> new RuntimeException("Book not found");
-        return repository.findById(id).orElseThrow(supplier);
+        return repository.getAll();
     }
 
     @PostMapping
     public Book create(@RequestBody Book book) {
         log.info("CREATE BOOK api called");
         return repository.save(book);
+    }
+
+    @DeleteMapping("/{id}")
+    public Long delete(@PathVariable String id) {
+        log.info("CREATE BOOK api called");
+        return repository.delete(id);
     }
 
 }
@@ -81,5 +93,35 @@ class Book implements Serializable {
 }
 
 
-interface BookRepository extends JpaRepository<Book, String> {
+@Repository
+class BookRepository {
+
+    public static final String BOOK_KEY = "books";
+    final RedisTemplate<String, Book> redisTemplate;
+    final HashOperations<String, String, Book> opsForHash;
+
+
+    BookRepository(RedisTemplate<String, Book> redisTemplate) {
+        this.redisTemplate = redisTemplate;
+        this.opsForHash = redisTemplate.opsForHash();
+    }
+
+
+    public Book save(@NonNull Book book) {
+        book.setId(UUID.randomUUID().toString());
+        opsForHash.put(BOOK_KEY, book.getId(), book);
+        return book;
+    }
+
+    public List<Book> getAll() {
+        return opsForHash.entries(BOOK_KEY).values().stream().toList();
+    }
+
+    public Long delete(@NonNull String id) {
+        return opsForHash.delete(BOOK_KEY, id);
+    }
+
+
 }
+
+
